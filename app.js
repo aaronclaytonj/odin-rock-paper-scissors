aa581ca74d5ff204c128eb354732840ae3d77b1b
Aaron Clayton <aaron.claytonj@outlook.com>
2023-04-21 21:05:35 -0500

    Added some UI to the RPS game
    added event listenrs to button to get player choice
    
    instead of console logging everything, adding text to divs to show on ui
    
    using new game method to reset lables, scores, hide certain buttons
    
    no css added yet, maybe something to do later

--- app.js
+++ app.js
@@ -1,21 +1,57 @@
 const CHOICES = ["Rock", "Paper", "Scissors"];
 
+const buttons = document.querySelectorAll(".choice");
+const playerScoreLabel = document.querySelector(".player-score");
+const computerScoreLabel = document.querySelector(".computer-score");
+const resLabel = document.querySelector(".result");
+const finalResultLabel = document.querySelector(".final-res");
+const playAgainBtns = document.querySelectorAll(".play-again");
+
+buttons.forEach((button) => button.addEventListener("click", playRound));
+playAgainBtns.forEach((button) => button.addEventListener("click", newGame));
+
+let playerScore = 0;
+let computerScore = 0;
+
+function newGame() {
+  playerScore = 0;
+  computerScore = 0;
+  playerScoreLabel.textContent = 0;
+  computerScoreLabel.textContent = 0;
+  playAgainBtns.forEach((button) => (button.hidden = true));
+  buttons.forEach((button) => (button.disabled = false));
+  finalResultLabel.textContent = null;
+  resLabel.textContent = null;
+}
+
+function playRound() {
+  let playerChoice = this.textContent.toLowerCase();
+  let computerChoice = getComputerChoice();
+  console.log(`player- ${playerChoice}  computer- ${computerChoice}`);
+  let res = determinRes(playerChoice, computerChoice);
+
+  if (res == 1) {
+    playerScore++;
+    playerScoreLabel.textContent = playerScore;
+  } else if (res == 0) {
+    computerScore++;
+    computerScoreLabel.textContent = computerScore;
+  }
+
+  if (playerScore == 5 || computerScore == 5) {
+    finalResultLabel.textContent =
+      playerScore == 5
+        ? "You win, would you like to play again?"
+        : "You lose, would you like to play again?";
+
+    buttons.forEach((button) => (button.disabled = true));
+    playAgainBtns.forEach((button) => (button.hidden = false));
+  }
+}
+
 function getComputerChoice() {
   let computerChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)];
   return computerChoice.toLowerCase();
-}
-
-function getPlayerInput() {
-  let computerChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)];
-  return computerChoice.toLowerCase();
-  //commented for speed rounds
-  let playerChoice = prompt("Please select Rock, Paper or Scissors");
-  while (!isValid(playerChoice)) {
-    playerChoice = prompt(
-      "OOPS, invalid. \nPlease select Rock, Paper or Scissors"
-    );
-  }
-  return playerChoice.toLowerCase();
 }
 
 function isValid(playerChoice) {
@@ -28,66 +64,70 @@ function isValid(playerChoice) {
   }
   return false;
 }
-
-function playRound(playerChoice, computerChoice) {
-  console.log(
-    `player - ${playerChoice} 
-computer - ${computerChoice}`
-  );
+function determinRes(playerChoice, computerChoice) {
   switch (playerChoice) {
     case "rock":
       if (computerChoice == "paper") {
-        return "you lose, paper beats rock";
+        resLabel.textContent = "you lose, paper beats rock";
+        return 0;
       } else if (computerChoice == "scissors") {
-        return "You win! Rock beats scissors.";
+        resLabel.textContent = "You win! Rock beats scissors.";
+        return 1;
       } else {
-        return "tie: rock and rock";
+        resLabel.textContent = "tie: rock and rock";
+        return 2;
       }
     case "paper":
       if (computerChoice == "scissors") {
-        return "You lose! Scissors beats paper.";
+        resLabel.textContent = "You lose! Scissors beats paper.";
+        return 0;
       } else if (computerChoice == "rock") {
-        return "You win! Paper beats rock.";
+        resLabel.textContent = "You win! Paper beats rock.";
+        return 1;
       } else {
-        return "tie: paper and paper";
+        resLabel.textContent = "tie: paper and paper";
+        return 2;
       }
     case "scissors":
       if (computerChoice == "rock") {
-        return "You lose! Rock beats scissors.";
+        resLabel.textContent = "You lose! Rock beats scissors.";
+        return 0;
       } else if (computerChoice == "paper") {
-        return "You win! Scissors beats paper.";
+        resLabel.textContent = "You win! Scissors beats paper.";
+        return 1;
       } else {
-        return "tie: scissors and scissors";
+        resLabel.textContent = "tie: scissors and scissors";
+        return 2;
       }
   }
 }
 
-function game() {
-  let i = 0;
-  let playerScore = 0;
-  let computerScore = 0;
-  let res = "Tie";
-  while (i < 5) {
-    let roundRes = playRound(getPlayerInput(), getComputerChoice());
-    console.log(roundRes);
-    if (roundRes.includes("lose")) {
-      computerScore++;
-    } else if (roundRes.includes("win")) {
-      playerScore++;
-    }
-    i++;
-  }
+// function game() {
+//   let i = 0;
+//   let playerScore = 0;
+//   let computerScore = 0;
+//   let res = "Tie";
+//   while (i < 5) {
+//     let roundRes = playRound(getPlayerInput(), getComputerChoice());
+//     console.log(roundRes);
+//     if (roundRes.includes("lose")) {
+//       computerScore++;
+//     } else if (roundRes.includes("win")) {
+//       playerScore++;
+//     }
+//     i++;
+//   }
 
-  if (playerScore > computerScore) {
-    res = "Win";
-  } else if (playerScore < computerScore) {
-    res = "Lose";
-  }
-  console.log(
-    res != "Tie"
-      ? `You ${res}! You had a score of: ${playerScore}. The computer had: ${computerScore}`
-      : `It was a ${res}! You had a score of: ${playerScore}. The computer had: ${computerScore}`
-  );
-}
+//   if (playerScore > computerScore) {
+//     res = "Win";
+//   } else if (playerScore < computerScore) {
+//     res = "Lose";
+//   }
+//   console.log(
+//     res != "Tie"
+//       ? `You ${res}! You had a score of: ${playerScore}. The computer had: ${computerScore}`
+//       : `It was a ${res}! You had a score of: ${playerScore}. The computer had: ${computerScore}`
+//   );
+// }
 
-game();
+//game();
